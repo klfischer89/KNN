29be7b367a3f637864969d6bc6de0b926f48fa32
klfischer89 <fischer-k4@web.de>
2025-12-16 13:25:00 +0100

    adding train test split for validation

--- grid_search_cv.py
+++ grid_search_cv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
-from sklearn.model_selection import GridSearchCV, RepeatedKFold
+from sklearn.model_selection import GridSearchCV, RepeatedKFold, train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
 df = pd.read_csv("./data/diabetes.csv")
@@ -9,16 +9,17 @@ df = pd.read_csv("./data/diabetes.csv")
 X = df[["BMI", "Age", "Glucose"]]
 y = df["Outcome"]
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.75)
+
 clf = GridSearchCV(DecisionTreeClassifier(), param_grid = {
     'max_depth': [3, 5, 10, 15, 20],
     'min_samples_leaf': [1, 3, 5, 10, 15]
 }, cv = RepeatedKFold())
 
-clf.fit(X, y)
+clf.fit(X_train, y_train)
 
 print(clf.best_params_)
 print(clf.best_score_)
 
-clf.predict(np.array([
-    [30, 25, 190]
-]))
+print(clf.score(X_train, y_train))
+print(clf.score(X_test, y_test))
